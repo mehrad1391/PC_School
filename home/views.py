64c0a6401074c65aa0b199440442601d3e6b4cbc
mehrad <mehradahsani4@gmail.com>
2025-09-24 23:49:48 +0330

    add something

--- home/views.py
+++ home/views.py
@@ -5,10 +5,10 @@ from rest_framework import filters
 from rest_framework import status
 from rest_framework.response import Response
 
-class CoursesListApiView(generics.ListAPIView):
+class CoursesListApiView(generics.ListCreateAPIView):
     queryset = Courses.objects.all()
     serializer_class = courseserializersapiview
 
-class SessionsListApiView(generics.ListAPIView):
+class SessionsListApiView(generics.ListCreateAPIView):
     queryset = session.objects.all()
     serializer_class = sessionsserializersapiview
