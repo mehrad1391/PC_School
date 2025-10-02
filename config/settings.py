52e71889276ec2600aa832d105b6afc02959a8bd
mehrad <mehradahsani4@gmail.com>
2025-10-02 21:46:20 +0330

    add swwager

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'home',
     'rest_framework',
+    'drf_spectacular',
 ]
 
 MIDDLEWARE = [
@@ -124,8 +125,17 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer',  
     ]
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Your Project API',
+    'DESCRIPTION': 'Your project description',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+    # OTHER SETTINGS
 }
--- home/urls.py
+++ home/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import *
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 urlpatterns = [
     path('Courses/', CoursesListApiView.as_view()),
@@ -10,4 +11,7 @@ urlpatterns = [
     path('session/<int:pk>', sessionApiview.as_view()),
     path('Courses/<int:pk>/edit/', CourseUpdateApiView.as_view(), name='course-edit'),
     path('sessions/<int:pk>/edit/', SessionUpdateApiView.as_view(), name='session-edit'),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
